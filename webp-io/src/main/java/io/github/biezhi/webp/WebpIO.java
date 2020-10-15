87d155a4d58189de51b3f4f9b5a87970b6c4738d
Vitaliy Sirotkin <vdsirotkin@dev.vtb>
2020-10-15 18:47:25 +0300

    start migrating to local webp binaries

--- webp-io/src/main/java/io/github/biezhi/webp/WebpIO.java
+++ webp-io/src/main/java/io/github/biezhi/webp/WebpIO.java
@@ -3,8 +3,9 @@ package io.github.biezhi.webp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.util.Objects;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
 
 /**
  * Webp converter
@@ -16,60 +17,8 @@ public class WebpIO {
 
     private static final Logger log = LoggerFactory.getLogger(WebpIO.class);
 
-    /**
-     * cwebp/dwebp/gif2webp
-     * <p>
-     * binary command file path
-     */
-    private static final String OS_NAME  = System.getProperty("os.name").toLowerCase();
-    private static final String OS_ARCH  = System.getProperty("os.arch").toLowerCase();
-    private static final String DEV_MODE = System.getProperty("webp-io.devMode", "false");
-
-    private String commandDir;
-    private String webpTmpDir = "cwebp_tmp";
-
-    public WebpIO() {
-        this.init();
-    }
-
-    private void init() {
-        String osName   = getOsName();
-        String webpPath = "cwebp/" + osName;
-
-        if (Boolean.TRUE.toString().equals(DEV_MODE.toUpperCase())) {
-            this.commandDir = WebpIO.class.getResource("/").getPath() + webpPath;
-            return;
-        }
-
-        // copy to tmp dir
-        File tmp = new File(webpTmpDir);
-        if (!tmp.exists()) {
-            tmp.mkdirs();
-        }
-
-        this.commandDir = tmp.getPath();
-
-        String extension = getExtensionByOs(osName);
-
-        InputStream dwebp    = getInputStream("/" + webpPath + "/dwebp" + extension);
-        InputStream cwebp    = getInputStream("/" + webpPath + "/cwebp" + extension);
-        InputStream gif2webp = getInputStream("/" + webpPath + "/gif2webp" + extension);
-
-        try {
-            copy(dwebp, new File(tmp.getPath() + "/dwebp" + extension));
-            copy(cwebp, new File(tmp.getPath() + "/cwebp" + extension));
-            copy(gif2webp, new File(tmp.getPath() + "/gif2webp" + extension));
-        } catch (Exception e) {
-            throw new WebpIOException(e);
-        }
-    }
-
     public static WebpIO create() {
         return new WebpIO();
-    }
-
-    private InputStream getInputStream(String subPath) {
-        return WebpIO.class.getResourceAsStream(subPath);
     }
 
     /**
@@ -93,7 +42,7 @@ public class WebpIO {
         for (String param : params) {
             paramsResult.append(param).append(" ");
         }
-        String command = commandDir + (dest.getName().endsWith(".gif") ? "/gif2webp" : "/dwebp ") + src.getPath() + " " + paramsResult.toString() + " -o " + dest.getPath();
+        String command = (dest.getName().endsWith(".gif") ? "gif2webp" : "dwebp ") + src.getPath() + " " + paramsResult.toString() + " -o " + dest.getPath();
         this.executeCommand(command);
     }
 
@@ -115,7 +64,7 @@ public class WebpIO {
      */
     public void toWEBP(File src, File dest) {
         try {
-            String command = commandDir + (src.getName().endsWith(".gif") ? "/gif2webp " : "/cwebp ") + src.getPath() + " -o " + dest.getPath();
+            String command = (src.getName().endsWith(".gif") ? "gif2webp " : "cwebp ") + src.getPath() + " -o " + dest.getPath();
             this.executeCommand(command);
         } catch (Exception e) {
             throw new WebpIOException(e);
@@ -147,65 +96,6 @@ public class WebpIO {
         if (!"".equals(output.toString())) {
             log.debug("Output: " + output);
         }
-        return "";
-    }
-
-    /**
-     * delete temp dir and commands
-     */
-    public void close() {
-        File tmp = new File(webpTmpDir);
-        if (tmp.exists() && tmp.isDirectory()) {
-            File[] files = tmp.listFiles();
-            for (File file : Objects.requireNonNull(files)) {
-                file.delete();
-            }
-            tmp.delete();
-        }
-    }
-
-    private void copy(InputStream in, File dest) throws IOException {
-        OutputStream out    = new FileOutputStream(dest);
-        byte[]       buffer = new byte[1024];
-        int          length;
-        //copy the file content in bytes
-        while ((length = in.read(buffer)) > 0) {
-            out.write(buffer, 0, length);
-        }
-        dest.setExecutable(true);
-        in.close();
-        out.close();
-    }
-
-    /**
-     * get os name and arch
-     *
-     * @return
-     */
-    private String getOsName() {
-        // windows
-        if (OS_NAME.contains("win")) {
-            boolean is64bit = (System.getenv("ProgramFiles(x86)") != null);
-            return "windows_" + (is64bit ? "x86_64" : "x86");
-        } else if (OS_NAME.contains("mac")) {
-            // mac osx
-            return "mac_" + OS_ARCH;
-        } else if (OS_NAME.contains("nix") || OS_NAME.contains("nux") || OS_NAME.indexOf("aix") > 0) {
-            // unix
-            return "amd64".equalsIgnoreCase(OS_ARCH) ? "linux_x86_64" : "linux_" + OS_ARCH;
-        } else {
-            throw new WebpIOException("Hi boy, Your OS is not support!!");
-        }
-    }
-
-    /**
-     * Return the Os specific extension
-     *
-     * @param os: operating system name
-     */
-    private String getExtensionByOs(String os) {
-        if (os == null || os.isEmpty()) return "";
-        else if (os.contains("win")) return ".exe";
         return "";
     }
 
